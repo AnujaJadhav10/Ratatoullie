6750717cfe7db6003dbe9e4bb014e2d571441ac1
Anuja Jadhav <Anuja.Jadhav@ril.com>
2024-03-18 16:34:11 +0530

    first commit

---
+++ Ratatoullie/Ratatoullie/src/main/java/com/rms/Ratatoullie/RatatoullieApplication.java
@@ -0,0 +1,13 @@
+package com.rms.Ratatoullie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatatoullieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatatoullieApplication.class, args);
+	}
+
+}
---
+++ Ratatoullie/Ratatoullie/src/test/java/com/rms/Ratatoullie/RatatoullieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rms.Ratatoullie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatatoullieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
